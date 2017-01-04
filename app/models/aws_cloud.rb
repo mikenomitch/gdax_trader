68c0ef3bda641f54e0db1fafc7419c48b44c3b25
Michael Nomitch <mnomitch@instructure.com>
2017-01-04 01:42:22 -0600

    added dockerignore and can get csv urls properly

--- app/models/aws_cloud.rb
+++ app/models/aws_cloud.rb
@@ -13,11 +13,19 @@ class AwsCloud
     def get_buckets
       s3 = Aws::S3::Client.new
       resp = s3.list_buckets
-      puts "whoa", resp.buckets.map(&:name)
+      resp.buckets.map(&:name)
     end
 
     def get_file_urls
-      get_buckets
+      base_s3_url = "https://s3.amazonaws.com/"
+      bucket = ENV['AWS_S3_CSV_BUCKET']
+      s3 = Aws::S3::Client.new
+      resp = s3.list_objects(bucket: bucket)
+
+      csv_urls = resp.contents.map do |obj|
+        base_s3_url + + "#{bucket}/" + obj.key.gsub(" ", "+")
+      end
+      puts 'csv_urls', csv_urls
     end
   end
 end
